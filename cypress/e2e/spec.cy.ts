d27486aee41cf7b2baf5f1d8814e243210b9b098
bones-bones <elliot.ma.brown@gmail.com>
2023-09-10 14:33:07 -0700

    init test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,11 @@
+// https://the-internet.herokuapp.com/
+
 describe("template spec", () => {
   it("passes", () => {
-    cy.visit("https://example.cypress.io");
+    cy.visit("https://the-internet.herokuapp.com/");
+    expect(cy.findByText("Forgot Password")).to.have.text("Forgot Password");
+    cy.findByText("Forgot Password").click();
   });
 });
+
+// cy.type('hello')
